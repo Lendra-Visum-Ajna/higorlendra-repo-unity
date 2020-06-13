85d329ac463eeea2b503fb5c9f2fe936aabbd075
Lendra Visum <higorfelipe127@gmail.com>
2020-06-13 17:18:27 -0300

    ProjectileController part. 2 Recu

--- Assets/Prefabs/Scripts/NavGame/Core/ProjectileController.cs
+++ Assets/Prefabs/Scripts/NavGame/Core/ProjectileController.cs
@@ -10,14 +10,23 @@ namespace NavGame.Core
     public class ProjectileController : MonoBehaviour
     {
 
+        public float tolerance = 0.1f;
+
+
         public float speed = 8f;
-
-
+        public OnAttackStrikeEvent onAttackStrike;
         DamageableGameObject target;
+        int damage;
+    bool isInit = false;
 
         void Start()
         {
+            if (!isInit)
+            {
+                Debug.LogError("ProjectController init was not called");
+                Destroy(gameObject);
 
+            }
         }
 
 
@@ -27,17 +36,31 @@ namespace NavGame.Core
             {
                 Destroy(gameObject);
                 return;
-        
+
             }
 
             transform.position = Vector3.MoveTowards(transform.position, target.damageTransform.position, speed * Time.deltaTime);
+            float distance = Vector3.Distance(transform.position, target.damageTransform.position);
+
+            if (distance < tolerance)
+            {
+                target.TakeDamage(damage);
+                Destroy(gameObject);
+                if (onAttackStrike != null)
+                {
+                    onAttackStrike(target.damageTransform.position);
+                }
+            
+            }
 
         }
 
-        public void Init(DamageableGameObject target)
+        public void Init(DamageableGameObject target, int damage)
         {
             this.target = target;
+            this.damage = damage;
+            isInit = true;
         }
-    }
+}
 
 }
