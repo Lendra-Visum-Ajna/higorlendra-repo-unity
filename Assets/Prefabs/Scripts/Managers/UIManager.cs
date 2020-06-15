358ea1a28cdc25f6a56784056dae81c18cd6a25c
Lendra Visum <higorfelipe127@gmail.com>
2020-06-15 18:02:22 -0300

    UI Manager

---
+++ Assets/Prefabs/Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] cooldownObjects;
+    Image[] cooldownImages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeUI();
+    }
+
+    // Update is called once per frame
+    void InitializeUI()
+    {
+        cooldownImages = new Image[cooldownObjects.Length];
+        for (int i = 0; i < cooldownObjects.Length; i++)
+        {
+            cooldownImages[i] = cooldownObjects[i].GetComponent<Image>();
+            cooldownImages[i].fillAmount = 0f;
+        }
+    }
+}
